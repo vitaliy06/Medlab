b9a1c3372a88847ea7fd60e2394f29406182d567
vitaliy06 <lichka119@gmail.com>
2023-03-16 13:03:59 +0500

    create pin and drawable circle

---
+++ app/src/main/java/com/example/medlab/pin.kt
@@ -0,0 +1,11 @@
+package com.example.medlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+    }
+}
